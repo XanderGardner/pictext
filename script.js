8d8e2364108b1b80d851a8a8e444b8f8cbce9403
Xander Gardner <xandergardner7@gmail.com>
2021-08-21 10:54:28 -0400

    Ability to read uploaded image pixel by pixel

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const file_input_element = document.getElementById('file-input');
-var imageData;
+const generate_button = document.getElementById('generate');
+const output_element = document.getElementById('output');
+var ctx;
+var width;
+var height;
 
 file_input_element.addEventListener('change', (e) => {
   const file = e.target.files[0];
   setInput(file);
+});
+
+generate_button.addEventListener('click', (e) => {
+  output_element.innerText = "hello there!\nHow are you?\nhehe";
 });
 
 // helpers
@@ -13,9 +21,10 @@ function setInput(file) {
   fr.onload = function () {
     var img = new Image
     img.onload = function () {
-      var ctx = document.createElement("canvas").getContext("2d");
+      ctx = document.createElement("canvas").getContext("2d");
       ctx.drawImage(img, 0, 0);
-      imageData = ctx.getImageData(0, 0, img.width, img.height).data;
+      width = img.width;
+      height = img.height;
       updateDisplay();
     };
     img.src = fr.result;
@@ -25,5 +34,18 @@ function setInput(file) {
 
 function updateDisplay() {
   
+}
+
+function getRed(row, col) {
+  return ctx.getImageData(col, row, 1, 1).data[0];
+}
+function getGreen(row, col) {
+  return ctx.getImageData(col, row, 1, 1).data[1];
+}
+function getBlue(row, col) {
+  return ctx.getImageData(col, row, 1, 1).data[2];
+}
+function getAlpha(row, col) {
+  return ctx.getImageData(col, row, 1, 1).data[3];
 }
 
